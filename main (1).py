9f932ca852083f4147adf38e7928a76c168f0f2e
CarterNotSteve <carterjw2005@gmail.com>
2022-05-06 11:08:15 -0500

    Working + mouse movement commit

--- main (1).py
+++ main (1).py
@@ -8,6 +8,11 @@ pos = (250,250)
 image = Image.open('pixil-frame-0.png')
 image = image.load()
 
+# half height walls
+# textures
+# minimap
+# a lil fella
+
 async def frame(time):
 	await asyncio.sleep(time)
 
@@ -30,8 +35,11 @@ pygame.init()
 
 surface = pygame.display.set_mode((500,500))
 deg = (math.pi/180)
+movable = True
 #pygame.draw.rect(surface, (255,0,0), pygame.Rect(0,0, 640, 640))
 player_angle = 0
+last_pos = (0,0)
+mvis = True
 
 while 1:
 	facingx = round(math.sin(player_angle*deg), 4)
@@ -43,19 +51,25 @@ while 1:
 		if event.type == pygame.MOUSEMOTION:
 			player_angle += event.rel[0]
 
-	pygame.draw.rect(surface, (255, 255, 255), [0,0,surface.get_height(), surface.get_width()])
+		if event.type == 768:
+			if event.key == 27:
+				mvis = not mvis
+		
 
-	if k[pygame.K_LEFT]:
-		player_angle -= 1
+	pygame.draw.rect(surface, (0, 0, 0), [0,0,surface.get_height(), surface.get_width()])
 
-	elif k[pygame.K_RIGHT]:
-		player_angle += 1
+	if movable:
+		if k[pygame.K_LEFT]:
+			player_angle -= 1
 
-	if k[pygame.K_w]:
-		pos = (pos[0]+facingy/2, pos[1]+facingx/2)
+		elif k[pygame.K_RIGHT]:
+			player_angle += 1
 
-	elif k[pygame.K_s]:
-		pos = (pos[0]-facingy/2, pos[1]-facingx/2)
+		if k[pygame.K_w]:
+			pos = (pos[0]+facingy/1.75, pos[1]+facingx/1.75)
+
+		elif k[pygame.K_s]:
+			pos = (pos[0]-facingy/1.75, pos[1]-facingx/1.75)
 
 	if player_angle == -1:
 		player_angle = 359
@@ -64,6 +78,18 @@ while 1:
 		player_angle = 0
 
 	size = 1
+
+	pygame.mouse.set_visible(not mvis)
+
+	d = pygame.mouse.get_pos()
+
+	if mvis:
+		print(d)
+		if d[0]>=480:
+			pygame.mouse.set_pos(16, d[1])
+
+		elif d[0] <= 15:
+			pygame.mouse.set_pos(479, d[1])
 	
 	for i in range(-45+player_angle, 45+player_angle):
 		# add 250 to x coordinate to draw in the middle at 0
@@ -86,16 +112,26 @@ while 1:
 			ri = 0
 
 		if ri != 0:
-			pcolor = (abs(255-ri*1.25), 0, abs(255-ri*1.25))
+			cclamp = abs(255-ri*1.5)
+			if cclamp < 0:
+				cclamp = 0
+
+			if cclamp > 255:
+				cclamp = 255
+
+			pcolor = (cclamp, 0, cclamp)
 		
 		else:
-			pcolor = (255,255,255)
+			pcolor = (0,0,0)
 				
 		
 		corner_threshold = 2
 
-		if ri < nri-corner_threshold or ri < lri-corner_threshold:
+		if ri < (nri/2)-corner_threshold or ri < (lri/2)-corner_threshold:
 			pcolor = (0, 0, 0)
+
+		if image[pos[0], pos[1]] == (0, 0, 0, 255):
+			pos = last_pos
 			
 			
 		pygame.draw.rect(surface, 
@@ -109,6 +145,7 @@ while 1:
 
 		pygame.event.pump()
 	
+	last_pos=pos
 	pygame.display.update()
 	
 	asyncio.run(frame(1/120))
